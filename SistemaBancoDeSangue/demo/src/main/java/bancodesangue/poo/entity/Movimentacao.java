939a8ef02bb6e4e57b88425eee2b2d24cdc3c3b7
Eberle-gio <giovanalopes@gmail.com>
2025-10-29 21:31:58 -0300

    feat: alteração da estratégia de herança da classe Movimentacao para TABLE_PER_CLASS

--- SistemaBancoDeSangue/demo/src/main/java/bancodesangue/poo/entity/Movimentacao.java
+++ SistemaBancoDeSangue/demo/src/main/java/bancodesangue/poo/entity/Movimentacao.java
@@ -15,7 +15,7 @@ import javax.persistence.InheritanceType;
 import bancodesangue.poo.enums.TipoSanguineo;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Movimentacao {
 
     @Id
