7c0e58ac7e6f26ed586549d677664f6c261a3412
Eberle-gio <giovanalopes@gmail.com>
2025-12-03 21:14:40 -0300

    feat: Implementação da classe JPAUtil para gerenciamento de persistência

---
+++ SistemaBancoDeSangue/demo/src/main/java/bancodesangue/poo/util/JPAUtil.java
@@ -0,0 +1,17 @@
+package bancodesangue.poo.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenciaPU");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public static void close() {
+        emf.close();
+    }
+}
