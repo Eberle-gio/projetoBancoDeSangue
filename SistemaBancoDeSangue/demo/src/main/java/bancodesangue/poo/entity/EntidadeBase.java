6c59f5873e9f29bf7184e7cc47e5f7843fc4f3d8
Eberle-gio <giovanalopes@gmail.com>
2025-12-03 21:15:48 -0300

    feat: Criação da classe EntidadeBase para gerenciamento de entidades base

---
+++ SistemaBancoDeSangue/demo/src/main/java/bancodesangue/poo/entity/EntidadeBase.java
@@ -0,0 +1,46 @@
+package bancodesangue.poo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class EntidadeBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 20)
+    private String telefone;
+
+    public abstract void validarCadastro();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
